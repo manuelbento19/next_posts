554cb12091cc1150d4b4b3a4089d62887a05553a
Manuel Bento <manuelbentomb.223@hotmail.com>
2024-02-21 13:58:17 +0100

    feat: Post view

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Post } from '../types'
+import Logo from '../_components/Logo'
+import Tag from '../_components/Tag'
 
 type Props = {
     params: {
@@ -10,16 +12,37 @@ type Props = {
 export default async function Page({params:{id}}:Props) {
 
     const request = await fetch(`https://dummyjson.com/posts/${id}`)
-    const data:Post = await request.json();
+    const post:Post = await request.json();
 
     return (
-        <div className="m-auto max-w-md w-full bg-white border shadow-sm p-5">
-            <dl className="-my-3 divide-y divide-gray-100 text-sm">
-                <div className="grid grid-cols-1 gap-1 py-3 even:bg-gray-50 sm:grid-cols-3 sm:gap-4">
-                    <dt className="font-medium text-gray-900">Title</dt>
-                    <dd className="text-gray-700 sm:col-span-2">{data.title}</dd>
-                </div>
-            </dl>
-        </div>
+        <section className='w-full h-full'>
+            <div className="mx-auto mt-[10%] max-w-lg w-full bg-white border shadow-sm p-5">
+                <dl className="my-5">
+                    <Logo/>
+                </dl>
+                <dl className="-my-3 divide-y divide-gray-100 text-sm">
+                    <div className="grid grid-cols-1 gap-1 py-3 even:bg-gray-50 sm:grid-cols-3 sm:gap-4">
+                        <dt className="font-medium text-gray-900">Title</dt>
+                        <dd className="text-gray-700 sm:col-span-2">{post.title}</dd>
+                    </div>
+                </dl>
+                <dl className="-my-3 divide-y divide-gray-100 text-sm">
+                    <div className="grid grid-cols-1 gap-1 py-3 even:bg-gray-50 sm:grid-cols-3 sm:gap-4">
+                        <dt className="font-medium text-gray-900">Description</dt>
+                        <dd className="text-gray-700 sm:col-span-2">{post.body}</dd>
+                    </div>
+                </dl>
+                {post.tags && (
+                <dl className="-my-3 divide-y divide-gray-100 text-sm">
+                    <div className="grid grid-cols-1 gap-1 py-3 even:bg-gray-50 sm:grid-cols-3 sm:gap-4">
+                        <dt className="font-medium text-gray-900">Tags</dt>
+                        <dd className="text-gray-700 sm:col-span-2">{post.tags.map((tag,index)=><Tag key={index} label={tag}/>)}</dd>
+                    </div>
+                </dl>
+                )}
+                
+                
+            </div>
+        </section>
     )
 }
--- app/_components/Logo.tsx
+++ app/_components/Logo.tsx
@@ -4,7 +4,7 @@ import { BiNews } from 'react-icons/bi'
 export default function Logo() {
   return (
     <div className="w-fit rounded bg-blue-600 p-2 text-white">
-        <BiNews size={22}/>
+      <BiNews size={22}/>
     </div>
   )
 }
